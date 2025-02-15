f01e7d35b3bf8cb4dc82f0119dcd3dad05f4fab7
John <johnsunday803@gmail.com>
2025-02-15 09:41:11 +0100

    New Commit

--- src/components/EquipmentDisplay.jsx
+++ src/components/EquipmentDisplay.jsx
@@ -50,8 +50,9 @@ const Equipment = () => {
     <div id="Equipment" className="p-4 bg-gray-100 min-h-screen">
       {/* Page Title */}
       <h1 className="text-3xl font-bold text-center text-gray-700 mb-6">
-        Equipment Gallery
+        Products Gallery
       </h1>
+
 
       {/* Search Bar */}
       <div className="flex justify-center mb-6">
